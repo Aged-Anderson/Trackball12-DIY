f6e8b958e42656e26573d3533e92c31206fa111d
Aged-Anderson <ianreynawillms@outlook.com>
2024-07-20 16:17:50 -0500

    Delete trackball directory
    not current files

--- trackball/halconf.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define HAL_USE_SPI TRUE
-
-#include_next "halconf.h"
--- trackball/mcuconf.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include_next "mcuconf.h"
-
-#undef RP_SPI_USE_SPI1
-#define RP_SPI_USE_SPI1 TRUE
